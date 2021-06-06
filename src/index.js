abae3967b22a4356051957b6fd437a9fb53df0d8
yuvanis <75557443+yuvanis@users.noreply.github.com>
2021-06-06 21:03:54 +0300

    feat: add code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse (n){
+    if (n < 0){
+        n *= (-1);
+    }
+    let str = String(n);
+    let arr = str.split('');
+    let newStr = '';
+    for (let i = arr.length - 1; i >= 0; i--){
+        newStr += arr[i];
+    }
+    return newStr;
 }
